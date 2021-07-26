9dacbb79295227af71099e80978902ae2e6dc31e
benleroter <benleroter@gmail.com>
2021-07-26 16:37:49 +0100

    edited for whitespace and naming standards

--- getdatafromcsv.py
+++ getdatafromcsv.py
@@ -1,12 +1,12 @@
 import csv
 
 def AddAirportToList(row, airportlist, count):
-	airportdetail = []
-	airportdetail.insert(0,row['NAME'])
-	airportdetail.insert(1,row['ICAO'])
-	airportdetail.insert(2,row['Latitude'])
-	airportdetail.insert(3,row['Longitude'])
-	airportlist.append(airportdetail)
+	airport_detail = []
+	airport_detail.insert(0,row['NAME'])
+	airport_detail.insert(1,row['ICAO'])
+	airport_detail.insert(2,row['Latitude'])
+	airport_detail.insert(3,row['Longitude'])
+	airportlist.append(airport_detail)
 
 def ExtractDataFromCSV(path):
 	airportlist = []
