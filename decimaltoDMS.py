03a8583dc14aa3426124dfac8dd78b213227699d
benleroter <benleroter@gmail.com>
2021-07-26 16:34:18 +0100

    edited for whitespace and naming standards

--- decimaltoDMS.py
+++ decimaltoDMS.py
@@ -1,31 +1,25 @@
-def LatitudeDecimaltoDMS(Decimal):
-    d = int(Decimal)
-    m = int((Decimal - d) * 60)
-    s = (Decimal - d - m/60) * 3600.00
-    z= round(s, 2)
+
+
+def LatitudeDecimaltoDMS(coordinate):
+    d = int(coordinate)
+    m = int((coordinate - d) * 60)
+    s= round((coordinate - d - m/60) * 3600.00, 2)
+
     if d >= 0:
-        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"N"
-        #print('DMS-Lat',DMS)
-        #print(str(abs(d))+"º "+str(abs(m))+"\'' "+str(abs(z))+'" '+"N")
+        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(s))+'" '+"N"
     else:
-        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"S"
-        #print('DMS-long',DMS)
-        #print(str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"S")
+        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(s))+'" '+"S"
 
     return DMS
 
-def LongitudeDecimaltoDMS(Decimal):
-    d = int(Decimal)
-    m = int((Decimal - d) * 60)
-    s = (Decimal - d - m/60) * 3600.00
-    z= round(s, 2)
-    if d >= 0:
-        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"E"
-        #print('DMS-Lat',DMS)
-        #print(str(abs(d))+"º "+str(abs(m))+"\'' "+str(abs(z))+'" '+"E")
-    else:
-        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"W"
-        #print('DMS-long',DMS)
-        #print(str(abs(d))+"º "+str(abs(m))+"' "+str(abs(z))+'" '+"W")
+def LongitudeDecimaltoDMS(coordinate):
+    d = int(coordinate)
+    m = int((coordinate - d) * 60)
+    s= round((coordinate - d - m/60) * 3600.00, 2)
 
+    if d >= 0:
+        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(s))+'" '+"E"
+    else:
+        DMS=str(abs(d))+"º "+str(abs(m))+"' "+str(abs(s))+'" '+"W"
+    
     return DMS
